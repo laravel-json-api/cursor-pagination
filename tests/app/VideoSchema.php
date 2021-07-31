7e3129bd572ef0b8da27bae764c049fcfb5b510c
Christopher Gammie <contact@gammie.co.uk>
2021-07-31 14:20:55 +0100

    [Feature] Bring in code from the Eloquent package

---
+++ tests/app/VideoSchema.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Copyright 2021 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace App;
+
+use LaravelJsonApi\Contracts\Pagination\Paginator;
+use LaravelJsonApi\Eloquent\Fields\DateTime;
+use LaravelJsonApi\Eloquent\Fields\ID;
+use LaravelJsonApi\Eloquent\Fields\Str;
+use LaravelJsonApi\Eloquent\Filters\Where;
+use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
+use LaravelJsonApi\Eloquent\Schema;
+
+class VideoSchema extends Schema
+{
+
+    /**
+     * The model the schema corresponds to.
+     *
+     * @var string
+     */
+    public static string $model = Video::class;
+
+    /**
+     * @inheritDoc
+     */
+    public function fields(): iterable
+    {
+        return [
+            ID::make()->uuid()->sortable(),
+            DateTime::make('createdAt')->sortable()->readOnly(),
+            Str::make('slug'),
+            Str::make('title'),
+            DateTime::make('updatedAt')->sortable()->readOnly(),
+            Str::make('url'),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function filters(): iterable
+    {
+        return [
+            WhereIdIn::make($this),
+            Where::make('slug')->singular(),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function pagination(): ?Paginator
+    {
+        return null;
+    }
+
+}
---
+++ tests/lib/Acceptance/TestCase.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Copyright 2021 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace LaravelJsonApi\CursorPagination\Tests\Acceptance;
+
+use LaravelJsonApi\Contracts\Schema\Container as SchemaContainerContract;
+use LaravelJsonApi\Contracts\Server\Server;
+use LaravelJsonApi\Core\Schema\Container as SchemaContainer;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+
+        $this->app->singleton(
+            SchemaContainerContract::class,
+            fn($container) => new SchemaContainer($container, $container->make(Server::class), [
+                \App\VideoSchema::class,
+            ])
+        );
+
+        $this->app->singleton(Server::class, function () {
+            $server = $this->createMock(Server::class);
+            $server->method('schemas')->willReturnCallback(fn() => $this->schemas());
+            return $server;
+        });
+    }
+
+    /**
+     * @return SchemaContainerContract
+     */
+    protected function schemas(): SchemaContainerContract
+    {
+        return $this->app->make(SchemaContainerContract::class);
+    }
+
+    /**
+     * @return Server
+     */
+    protected function server(): Server
+    {
+        return $this->app->make(Server::class);
+    }
+}
